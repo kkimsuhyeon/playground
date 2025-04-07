89d8ac245178d45c22c3cb312e1f9730816dcda0
kkimsuhyeon <wzxy2002@naver.com>
2025-04-07 23:57:27 +0900

    config, filter 생성

---
+++ api-gateway/src/main/java/hayashi/apigateway/filter/AuthenticationFilter.java
@@ -0,0 +1,27 @@
+package hayashi.apigateway.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+
+    private final ObjectMapper objectMapper;
+
+    public AuthenticationFilter(ObjectMapper objectMapper) {
+        super(Config.class);
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return null;
+    }
+
+
+    public static class Config {
+    }
+
+}
