7d8b6e0d51f4b983c0095846042ebc98951728b4
kkimsuhyeon <wzxy2002@naver.com>
2025-04-18 16:24:34 +0900

    불필요한 코드 제거

--- api-gateway/src/main/java/hayashi/apigateway/config/SwaggerConfig.java
+++ api-gateway/src/main/java/hayashi/apigateway/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package hayashi.apigateway.config;
 
-import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -14,14 +13,6 @@ public class SwaggerConfig {
     public RouteLocator swaggerRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(r -> r.path("/user-service/**").and().method(HttpMethod.GET).uri("lb://user-service"))
-                .build();
-    }
-
-    @Bean
-    public GroupedOpenApi gatewayApi() {
-        return GroupedOpenApi.builder()
-                .group("gateway-api")
-                .pathsToMatch("/gateway/**")
                 .build();
     }
 }
