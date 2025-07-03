ec7ac104c416d75c5cf4b8af4d9687f635ef6e16
kkimsuhyeon <wzxy2002@naver.com>
2025-07-03 17:34:17 +0900

    코드리뷰 수정사항 반영

--- infrastructure/mongodb/mongo-init.js
+++ infrastructure/mongodb/mongo-init.js
@@ -1,14 +1,14 @@
 
 print("MongoDB initialization script running...");
 
-db = db.getSiblingDB("api_log_db");
-print("MongoDB database created: api_log_db");
+db = db.getSiblingDB("log_db");
+print("MongoDB database created: log_db");
 
 db.createUser({
-    user: process.env.MONGO_INITDB_API_LOG_USER,
-    pwd: process.env.MONGO_INITDB_API_LOG_PASSWORD,
-    roles: [{ role: "dbOwner", db: "api_log_db" }]
+    user: process.env.MONGO_INITDB_LOG_USER,
+    pwd: process.env.MONGO_INITDB_LOG_PASSWORD,
+    roles: [{ role: "dbOwner", db: "log_db" }]
 });
-print("MongoDB user created: user with dbOwner role on api_log_db");
+print("MongoDB user created: user with dbOwner role on log_db");
 
 db.createCollection("logs");
