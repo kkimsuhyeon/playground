6d2d68fbd4a0bb1034873e6ef7ef4c7dd0f93d50
kkimsuhyeon <wzxy2002@naver.com>
2025-07-08 17:10:36 +0900

    logging 적용

--- user-service/src/main/java/hayashi/userservice/config/client/FeignClientConfig.java
+++ user-service/src/main/java/hayashi/userservice/config/client/FeignClientConfig.java
@@ -1,17 +1,18 @@
 package hayashi.userservice.config.client;
 
 import feign.RequestInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 public class FeignClientConfig {
 
     @Bean
     public RequestInterceptor requestInterceptor() {
         return requestTemplate -> {
-            System.out.println("요청 URL: " + requestTemplate.url());
-            System.out.println("요청 메소드: " + requestTemplate.method());
+            log.info("요청 URL : {}, 요청 메소드: {}", requestTemplate.url(), requestTemplate.method());
         };
     }
 
