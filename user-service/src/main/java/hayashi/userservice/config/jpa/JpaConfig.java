a175ce4e3e37204eeeaeca2fa6c2a34e291865d6
kkimsuhyeon <wzxy2002@naver.com>
2025-07-09 17:31:10 +0900

    SecurityUtil 적용

--- user-service/src/main/java/hayashi/userservice/config/jpa/JpaConfig.java
+++ user-service/src/main/java/hayashi/userservice/config/jpa/JpaConfig.java
@@ -1,13 +1,10 @@
 package hayashi.userservice.config.jpa;
 
-import hayashi.userservice.config.security.AuthUser;
+import hayashi.userservice.shared.util.SecurityUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
@@ -17,15 +14,7 @@ public class JpaConfig {
 
     @Bean
     public AuditorAware<String> auditorAware() {
-        return () -> Optional.ofNullable(SecurityContextHolder.getContext())
-                .map(SecurityContext::getAuthentication)
-                .filter(Authentication::isAuthenticated)
-                .map(Authentication::getPrincipal)
-                .map(principal -> {
-                    if (principal instanceof AuthUser authUser) return authUser.getUsername();
-                    return "SYSTEM";
-                })
-                .or(() -> Optional.of("SYSTEM"));
+        return () -> Optional.ofNullable(SecurityUtils.getCurrentUserId());
     }
 
 }
