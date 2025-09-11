c6dd221b9a82e981ca2841a48670085f1623a9ff
kkimsuhyeon <wzxy2002@naver.com>
2025-09-11 13:38:23 +0900

    코드리뷰 수정사항 반영

--- user-service/src/main/java/hayashi/userservice/shared/jpa/SpecificationUtils.java
+++ user-service/src/main/java/hayashi/userservice/shared/jpa/SpecificationUtils.java
@@ -12,21 +12,21 @@ public final class SpecificationUtils {
 
     public static <T> Specification<T> likeIgnoreCase(String fieldName, String value) {
         return (root, query, criteriaBuilder) -> {
-            if (!StringUtils.hasText(value)) return criteriaBuilder.conjunction();
+            if (!StringUtils.hasText(value)) return null;
             return criteriaBuilder.like(criteriaBuilder.lower(root.get(fieldName)), "%" + value.toLowerCase() + "%");
         };
     }
 
     public static <T> Specification<T> equalTo(String fieldName, Object value) {
         return (root, query, criteriaBuilder) -> {
-            if (value == null) return criteriaBuilder.conjunction();
+            if (value == null) return null;
             return criteriaBuilder.equal(root.get(fieldName), value);
         };
     }
 
     public static <T> Specification<T> in(String fieldName, Collection<?> values) {
         return (root, query, criteriaBuilder) -> {
-            if (values == null || values.isEmpty()) return criteriaBuilder.conjunction();
+            if (values == null || values.isEmpty()) return null;
             return root.get(fieldName).in(values);
         };
     }
